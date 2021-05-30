895adfa24f89adce519b6da3b554eb62d90fd37e
Artem Stepanyuk <aivanov757@gmail.com>
2021-05-30 16:55:58 +0300

    Выполнено первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,15 @@
 function solveEquation(a, b, c) {
   let arr;
+  let d = b**2 - 4*a*c;
+  let x1;
+  let x2;
+  if (d >= 0) {
+    x1 = (-b + Math.sqrt(d)/(2*a)
+    arr.push(x1);
+  } if (d > 0) {
+    x2 = (-b - Math.sqrt(d)/(2*a)
+    arr.push(x2);
+  }
   // код для задачи №1 писать здесь
   return arr; // array
 }
