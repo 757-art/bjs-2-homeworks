1dfcbee6f7fd9feac9581c82820987bcedcf6d48
Artem Stepanyuk <aivanov757@gmail.com>
2021-06-05 21:04:54 +0300

    Second HW Functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,26 +1,48 @@
 // Задание 1
 function getArrayParams(arr) {
   let min,max,sum,avg;
-
-  // Ваш код
+  min = 101;
+  max = -101;
+  sum = 0;
+  
+  for (let i = 0;  i < arr.length; i++ ) {
+    sum += arr[i];
+    avg = (sum/arr.length).toFixed(2);
+  if (arr[i] > max) {
+    max = arr[i]
+  }
+  if (arr[i] < min) {
+    min = arr[i]
+  }
+}
+   
 
   return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
+  for (let i = 0;  i < arr.length; i++ ) {
+    sum += arr[i];
+  }
 
-  // Ваш код
+ 
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
-
-  // Ваш кода
-  // for ...
+  let max = 0;
+  
+  
+  for (let i = 0;  i < arrOfArr.length; i++ ) {
+    let element = arrOfArr[i];
+    worker(element);
+    if (worker(element) > max) {
+      max = worker(element)
+    }
+  } 
   
   return max
 }
