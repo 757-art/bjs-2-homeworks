9adc527d5f87740cab2ab15f0d89710ccba85340
Artem Stepanyuk <aivanov757@gmail.com>
2021-06-06 12:25:28 +0300

    thirdHwArrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  
+  result = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
+  const arrPositive = arr.filter(value => value > 0);
+  const arr2 =  arrPositive.filter(value => !(value % 3));
+  resultArr = arr2.map((value) => value * 10); 
+  
+  
 
   return resultArr; // array
 }
