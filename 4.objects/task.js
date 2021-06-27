4bdc9d2ecf1b4b83b5a970d1139de8cd709ece9f
Artem Stepanyuk <aivanov757@gmail.com>
2021-06-27 13:31:26 +0300

    Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,40 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
+let student1 = new Student("Tony", "male", 37);
+student1.setSubject("Algebra");
+student1.addMark(5);
+student1.addMark(4);
+student1.addMark(5);
+console.log(student1);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+  marks.forEach((item) => this.marks.push(item));
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+  marks.forEach((item) => this.marks.push(item));
+}
+
+Student.prototype.getAverage = function () {
+  let sum = this.marks.reduce((acc, item) => acc + item, 0);
+  return Number((sum/this.marks.length).toFixed(1));
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.exclude = reason;
+}
