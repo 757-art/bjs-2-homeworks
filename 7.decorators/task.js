e60ce4c30a02c0b2da0ba26ec9ea0d5c2e8838fd
Artem Stepanyuk <aivanov757@gmail.com>
2021-08-23 11:38:33 +0300

    Первое задание

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,15 +1,22 @@
-function compareArrays(arr1, arr2) {
-  let result;
+function decorator(func) {
+  let cache = [];
 
-  // Ваш код
+  function wrapper(...args) {
+    const hash = args.join(',');
 
-  return result; // boolean
-}
-
-function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+    let idx = cache.findIndex((item) => hash === item.hash);
+    if (idx !== -1) {
+      console.log("Из кэша: " + cache[idx].value);
+      return "Из кэша: " + cache[idx].value;
+    } else {
+      let result = func(...args);
+      cache.push({hash: hash, value: result});
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  }
+  return wrapper;
 }
